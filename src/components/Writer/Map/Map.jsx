139c6a5ba30f7a4d271c51f5ac65e16b87627c48
talalaev <talalaev4@mail.ru>
2022-12-13 01:21:44 +0300

    Update Map.jsx

--- src/components/Writer/Map/Map.jsx
+++ src/components/Writer/Map/Map.jsx
@@ -19,7 +19,7 @@ const OwnMap = (props) => {
         <div>
             <h1>{t('place_of_work')}</h1>
             <YMaps>
-                <Map defaultState={defaultState} modules={yandexModules} width="100%" height='300'>
+                <Map defaultState={defaultState} modules={yandexModules} width="100%" height="60vh">
                     <Placemark
                         geometry={[props.coordinates.latitude, props.coordinates.longitude]}/>
                 </Map>
